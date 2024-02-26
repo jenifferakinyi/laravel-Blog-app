bad032f57a494654f93d3e1f26fa734b31e30abd
jenifferakinyi <otigojeniffer19@gmail.com>
2024-02-26 09:44:20 +0300

    adding blade template

--- resources/views/listing.php
+++ resources/views/listing.php
@@ -8,6 +8,9 @@
 
 blade version
 <h1>{{ $heading }}</h1>
+@if(count($listings)==0)
+<p>no listings</p>
+@endif
 @foreach($listings as $listings)
 <h2>{{ $listings['title']}}</h2>
 <p>{{ $listings['description']}}</p>
