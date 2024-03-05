e0629e4a760ee42517a27d6c0115c483d55a37a2
jenifferakinyi <otigojeniffer19@gmail.com>
2024-03-05 07:38:59 +0300

    adding new file

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,31 +2,10 @@
 
 namespace App\Models;
 
-class Listing {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'listing one',
-                'description' => 'removing the welcome blade content removing the welcome blade content removing the welcome blade content'
-            ],
-            [
-                'id' => 2,
-                'title' => 'listing two',
-                'description' => 'removing the welcome blade content removing the welcome blade content removing the welcome blade content'
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-
-        return null;
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
